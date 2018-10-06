2c021bf36e9fcf20391cc5963239377ee2fc63d6
David on big surface <davidsomerville@comcast.net>
2018-10-05 21:13:33 -0400

    Initial version of checkListClient using ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { FooterbarComponent } from './footerbar/footerbar.component';
+import { NavigationbarComponent } from './navigationbar/navigationbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SigninComponent,
+    SignupComponent,
+    NotfoundComponent,
+    NavigationbarComponent,
+    FooterbarComponent,
+    FooterbarComponent,
+    NavigationbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'signin', component: SigninComponent },
+      { path: 'signup', component: SignupComponent },
+      { path: '**', component: NotfoundComponent },
+    ])
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navigationbar/navigationbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit ,Input} from '@angular/core';
+
+@Component({
+  selector: 'navigationbar',
+  templateUrl: './navigationbar.component.html',
+  styleUrls: ['./navigationbar.component.css']
+})
+export class NavigationbarComponent implements OnInit {
+
+  state: number;
+  @Input() isPublisher: boolean;
+  @Input() isSignedIn: boolean
+  constructor() { console.log('Navigator constuctor'); }
+
+  ngOnInit() {
+
+    console.log('Navigator OnInit: ' + this.state);
+    this.state += 1;
+
+  }
+
+}
