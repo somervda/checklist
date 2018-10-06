67a1690ef634fa7dcbb8e90f6740d1684014a881
David  on Go <davidsomerville@comcast.net>
2018-10-06 18:06:48 -0400

    updated images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-  // Note: use of backticks or template-literals
-  images = [1, 2, 3].map(
-    () => `https://picsum.photos/900/500?random&t=${Math.random()}`
-  );
+
 
   constructor() {}
 
