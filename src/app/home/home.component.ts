150ac4d35850be522775cd5f99bd07c6f01b9426
David on big surface <davidsomerville@comcast.net>
2018-10-06 08:29:42 -0400

    Fixed navbar collapse toggle, cleaned up home and colors

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,11 +6,12 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()});
-  
+  // Note: use of backticks or template-literals
+  images = [1, 2, 3].map(
+    () => `https://picsum.photos/900/500?random&t=${Math.random()}`
+  );
+
   constructor() {}
 
-  ngOnInit() {
-    
-  }
+  ngOnInit() {}
 }
--- src/app/navigationbar/navigationbar.component.ts
+++ src/app/navigationbar/navigationbar.component.ts
@@ -1,22 +1,23 @@
-import { Component, OnInit ,Input} from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'navigationbar',
-  templateUrl: './navigationbar.component.html',
-  styleUrls: ['./navigationbar.component.css']
+  selector: "navigationbar",
+  templateUrl: "./navigationbar.component.html",
+  styleUrls: ["./navigationbar.component.css"]
 })
 export class NavigationbarComponent implements OnInit {
-
   state: number;
-  @Input() isPublisher: boolean;
-  @Input() isSignedIn: boolean
-  constructor() { console.log('Navigator constuctor'); }
-
-  ngOnInit() {
-
-    console.log('Navigator OnInit: ' + this.state);
-    this.state += 1;
-
+  @Input()
+  isPublisher: boolean;
+  @Input()
+  isSignedIn: boolean;
+  isNavbarCollapsed = true;
+  constructor() {
+    console.log("Navigator constuctor");
   }
 
+  ngOnInit() {
+    console.log("Navigator OnInit: " + this.state);
+    this.state += 1;
+  }
 }
