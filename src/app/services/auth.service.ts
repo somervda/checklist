34623845fce5dade2495c9473fd6ae75af4213ec
somervda on dev pc <davidsomerville@comcast.net>
2018-10-25 12:45:23 -0400

    Updated icons and option for Auth0 dialog

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,8 +9,12 @@ import Auth0Lock from "auth0-lock";
 export class AuthService {
   auth0Options = {
     theme: {
-      logo: "assets/images/mcl-big-58x60.png",
-      primaryColor: "#DFA612"
+      logo: "assets/images/Logo-small.png",
+      primaryColor: "	#006f00"
+    },
+    // See options at https://github.com/auth0/lock/blob/master/src/i18n/en.js
+    languageDictionary: {
+      title: "myCheckLists.com"
     },
     auth: {
       redirectUrl: environment.auth0.callbackURL,
