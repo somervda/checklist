4cc2e43a1a010ec0508d1e5b5283e2b4b22657df
David  on Go <davidsomerville@comcast.net>
2018-11-05 15:01:26 -0500

    Working roles with cleaner code

--- src/app/services/session.store.service.ts
+++ src/app/services/session.store.service.ts
@@ -12,20 +12,19 @@ export class SessionStore {
     // Store session information in LocalStorage 
     // this will preserve the information 
 
-    // app_metadata must not be changable so store that as the encoded accessToken
-
-    
+    // app_metadata must not be changeable on the client so store that as the encoded accessToken
+    // this includes role info (it would be bad if role for a user can be changed by the user)
+    // user_metadata is ok to be stored in plain text in local storage
+  
 
     public isInRole(roleName) {
      
         if (localStorage.getItem("accessToken"))
         {
             const decodedAccessToken = helper.decodeToken(localStorage.getItem("accessToken"));
-            console.log("decodedAccessToken",decodedAccessToken);
             if (decodedAccessToken[environment.auth0.apiNameSpace  + '/roles'])
                 {
                 const roles: String[] = decodedAccessToken[environment.auth0.apiNameSpace  + '/roles'];
-                console.log("roles",roles);
                 return (roles.indexOf(roleName) > -1);
                 }
         }
