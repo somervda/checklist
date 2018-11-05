a9bf36af693a89bac6aac1fad7a67a8784961dd4
David on big surface <davidsomerville@comcast.net>
2018-11-05 15:49:03 -0500

    Changed injected services to public

--- src/app/navigationbar/navigationbar.component.ts
+++ src/app/navigationbar/navigationbar.component.ts
@@ -2,20 +2,16 @@ import { Component, OnInit, Input } from "@angular/core";
 import { AuthService } from "../services/auth.service";
 import { SessionStore } from "../services/session.store.service";
 
-
 @Component({
   selector: "navigationbar",
   templateUrl: "./navigationbar.component.html",
   styleUrls: ["./navigationbar.component.css"]
 })
 export class NavigationbarComponent implements OnInit {
-
   // see https://embed.plnkr.co/plunk/xH6VJo for example of a ngb menu
   isNavbarCollapsed = true;
 
-  constructor(private auth: AuthService, private sessionStore: SessionStore) { }
+  constructor(public auth: AuthService, public sessionStore: SessionStore) {}
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 }
