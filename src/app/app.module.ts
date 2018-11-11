5e2d38d66789eea8aad7e8032ff20d1aec2ae6c8
David on big surface <davidsomerville@comcast.net>
2018-11-11 16:12:31 -0500

    added ngx-datatable and httpClient modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ToastrModule } from "ngx-toastr";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
-import { NgbDropdownModule, NgbCarouselModule, NgbCollapseModule } from "@ng-bootstrap/ng-bootstrap";
+import {
+  NgbDropdownModule,
+  NgbCarouselModule,
+  NgbCollapseModule
+} from "@ng-bootstrap/ng-bootstrap";
 import { HomeComponent } from "./home/home.component";
 import { NotfoundComponent } from "./notfound/notfound.component";
 import { FooterbarComponent } from "./footerbar/footerbar.component";
 import { NavigationbarComponent } from "./navigationbar/navigationbar.component";
 import { SessionStore } from "./services/session.store.service";
-import { UserprofileComponent } from './userprofile/userprofile.component';
-import { UsersettingsComponent } from './usersettings/usersettings.component';
-import { MychecklistComponent } from './mychecklist/mychecklist.component';
+import { UserprofileComponent } from "./userprofile/userprofile.component";
+import { UsersettingsComponent } from "./usersettings/usersettings.component";
+import { MychecklistComponent } from "./mychecklist/mychecklist.component";
+import { NgxDatatableModule } from "@swimlane/ngx-datatable";
 
 @NgModule({
   declarations: [
@@ -32,10 +38,12 @@ import { MychecklistComponent } from './mychecklist/mychecklist.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule, // required animations module
+    HttpClientModule,
     ToastrModule.forRoot(), // ToastrModule added
     NgbDropdownModule,
     NgbCarouselModule,
     NgbCollapseModule,
+    NgxDatatableModule,
     RouterModule.forRoot([
       { path: "", component: HomeComponent },
       { path: "userprofile", component: UserprofileComponent },
@@ -47,4 +55,4 @@ import { MychecklistComponent } from './mychecklist/mychecklist.component';
   providers: [AuthService, SessionStore],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/mychecklist/mychecklist.component.ts
+++ src/app/mychecklist/mychecklist.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'mychecklist',
-  templateUrl: './mychecklist.component.html',
-  styleUrls: ['./mychecklist.component.css']
+  selector: "mychecklist",
+  templateUrl: "./mychecklist.component.html",
+  styleUrls: ["./mychecklist.component.css"]
 })
 export class MychecklistComponent implements OnInit {
+  rows = [
+    { name: "Austin", gender: "Male", company: "Swimlane" },
+    { name: "Dany", gender: "Male", company: "KFC" },
+    { name: "Molly", gender: "Female", company: "Burger King" }
+  ];
+  columns = [{ prop: "name" }, { name: "Gender" }, { name: "Company" }];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
