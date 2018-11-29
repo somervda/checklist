ca9a7063eb090242e5d4a6d68e89c2fb9375ffd7
David on big surface <davidsomerville@comcast.net>
2018-11-29 16:55:09 -0500

    Working firestore page mychecklists except for realtime

--- src/app/mychecklists/mychecklists.component.ts
+++ src/app/mychecklists/mychecklists.component.ts
@@ -1,21 +1,26 @@
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { Component, OnInit } from '@angular/core';
-import { FirebaseStorage } from '@angular/fire';
-import { AngularFirestore } from '@angular/fire/firestore'
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { Component, OnInit } from "@angular/core";
+import { FirebaseStorage } from "@angular/fire";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
-  selector: 'app-mychecklists',
-  templateUrl: './mychecklists.component.html',
-  styleUrls: ['./mychecklists.component.css']
+  selector: "app-mychecklists",
+  templateUrl: "./mychecklists.component.html",
+  styleUrls: ["./mychecklists.component.css"]
 })
 export class MychecklistsComponent implements OnInit {
+  checklists = [];
 
-  constructor(private db: AngularFirestore) { }
+  constructor(private db: AngularFirestore) {}
 
   ngOnInit() {
-    this.db.collection("/checklists").get().subscribe(data => console.log("Chacklists data", data));
-    //this.db.list("/checklists").valueChanges   
-
+    this.db
+      .collection("/checklists")
+      .get()
+      .subscribe(snapshot => {
+        this.checklists = snapshot.docs;
+        console.log("Checklists docs", snapshot.docs);
+      });
+    //this.db.list("/checklists").valueChanges
   }
-
 }
