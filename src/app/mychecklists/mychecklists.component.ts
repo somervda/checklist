6625b33609740c5802673ff58f04de244aafee53
David on big surface <davidsomerville@comcast.net>
2018-11-29 20:13:51 -0500

    Realtime updates working

--- src/app/mychecklists/mychecklists.component.ts
+++ src/app/mychecklists/mychecklists.component.ts
@@ -1,26 +1,33 @@
 import { AngularFirestoreModule } from "@angular/fire/firestore";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { FirebaseStorage } from "@angular/fire";
 import { AngularFirestore } from "@angular/fire/firestore";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-mychecklists",
   templateUrl: "./mychecklists.component.html",
   styleUrls: ["./mychecklists.component.css"]
 })
-export class MychecklistsComponent implements OnInit {
+export class MychecklistsComponent implements OnInit, OnDestroy {
   checklists = [];
+  checklistSubscription: Subscription;
 
   constructor(private db: AngularFirestore) {}
 
   ngOnInit() {
-    this.db
+    // See https://www.udemy.com/the-complete-angular-master-class/learn/v4/t/lecture/7673810?start=0
+    this.checklistSubscription = this.db
       .collection("/checklists")
-      .get()
+      .snapshotChanges()
       .subscribe(snapshot => {
-        this.checklists = snapshot.docs;
-        console.log("Checklists docs", snapshot.docs);
+        this.checklists = snapshot;
+        console.log("Checklists docs", snapshot);
       });
     //this.db.list("/checklists").valueChanges
+  }
+
+  ngOnDestroy() {
+    this.checklistSubscription.unsubscribe();
   }
 }
