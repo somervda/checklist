39723d49261aa963e793c28c8062d61f24487bbd
David on big surface <davidsomerville@comcast.net>
2018-12-01 09:33:56 -0500

    used pipe.map to transform snapshotchanges observable to something more usable

--- src/app/mychecklists/mychecklists.component.ts
+++ src/app/mychecklists/mychecklists.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { AngularFirestore } from "@angular/fire/firestore";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: "app-mychecklists",
@@ -8,12 +10,41 @@ import { AngularFirestore } from "@angular/fire/firestore";
 })
 export class MychecklistsComponent implements OnInit {
   checklists$;
+  checklists;
+
+  columns = [{ prop: "title", name: "Id" }, { prop: "author", name: "Title" }];
 
   constructor(private db: AngularFirestore) {}
 
   ngOnInit() {
     // See https://www.udemy.com/the-complete-angular-master-class/learn/v4/t/lecture/7673810?start=0
     // Using observable and async to manage lifetime of subscription in sync with lifetime of the component
-    this.checklists$ = this.db.collection("/checklists").snapshotChanges();
+
+    // use map operator to normaize snapshotchanges to a more easily managed array of objects that have document id  // and data at same level
+    // see https://stackoverflow.com/questions/48795092/angular-httpclient-map-observable-array-of-objects
+
+    this.checklists$ = this.db.collection("/checklists").valueChanges();
+
+    this.db
+      .collection("/checklists")
+      .snapshotChanges()
+      .pipe(
+        map(data =>
+          data.map(row => {
+            return Object.assign(
+              { id: row.payload.doc.id },
+              row.payload.doc.data()
+            );
+            // return {
+            //   xmas: "25"
+            // };
+            console.log("row", row);
+          })
+        )
+      )
+      .subscribe(snapshot => {
+        this.checklists = snapshot;
+        console.log("snapshot: ", snapshot);
+      });
   }
 }
