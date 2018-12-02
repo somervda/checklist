436c46b2159d7dfaf03fcdbabef4a12b5676dd5a
somervda on dev pc <davidsomerville@comcast.net>
2018-12-02 12:35:25 -0500

    Simple checklist display working

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-checklist',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChecklistComponent implements OnInit {
 
-  constructor() { }
+  checklist$;
+
+  constructor(private route: ActivatedRoute,
+    private db: AngularFirestore) { }
 
   ngOnInit() {
-  }
+    this.route.paramMap.subscribe(paramMap => {
+      var id = paramMap.get('id');
+      this.checklist$ = this.db.doc("/checklists/" + id)
+        .get();
+      // .subscribe(data =>
+      //   console.log(data.data()));
+    });
 
+  };
 }
+
