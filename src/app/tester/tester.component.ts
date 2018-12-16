73ec4c233006412b6cb3ec5985819c7916a4ac20
David on big surface <davidsomerville@comcast.net>
2018-12-16 17:08:33 -0500

    Tested approuch to user community data structure.

--- src/app/tester/tester.component.ts
+++ src/app/tester/tester.component.ts
@@ -8,29 +8,42 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./tester.component.css"]
 })
 export class TesterComponent implements OnInit {
-  users$;
+  users;
+  communityId: string = "Tw8CPGkAwTxjUxW7dnNg";
+  //communityId: string = "Ufrqt16S1Yr1c7cG7eqq";
 
   constructor(private db: AngularFirestore) {}
 
   ngOnInit() {
-    this.db
-      .collection("users", ref =>
-        ref.where(
-          "communities",
-          "array-contains",
-          "{ community:'Tw8CPGkAwTxjUxW7dnNg',roleState:'member'}"
-        )
-      )
-      .get()
-      .toPromise()
-      .then(data => console.log("array of maps test", data));
+    const accessField: string =
+      "communities." + this.communityId + ".accessState";
+
+    // this.db
+    //   .collection("users", ref =>
+    //     ref.where(
+    //       "communities",
+    //       "array-contains",
+    //       "{ community:'Tw8CPGkAwTxjUxW7dnNg',roleState:'member'}"
+    //     )
+    //   )
+    //   .get()
+    //   .toPromise()
+    //   .then(data => console.log("array of maps test", data));
 
     this.db
-      .collection("users", ref =>
-        ref.where("memberOfCommunity", "array-contains", "Tw8CPGkAwTxjUxW7dnNg")
+      .collection(
+        "users",
+        ref =>
+          ref
+            //         .where("memberOfCommunity", "array-contains", this.communityId)
+            // .where(accessField, "==", "member") // Find users who have member access to the community
+            .where(accessField, ">", "") // Will find users who have any access to community
       )
       .get()
       .toPromise()
-      .then(data => console.log("array of strings test", data));
+      .then(data => {
+        console.log(" docs collection", data.docs);
+        this.users = data.docs;
+      });
   }
 }
