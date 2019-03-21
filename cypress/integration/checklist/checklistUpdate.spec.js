c51a42e50f7f83f74aadc1b58737df0995798adf
somervda on dev pc <davidsomerville@comcast.net>
2019-03-21 11:18:35 -0400

    Updated cypress and npm versions, fixed the update script so that the header is opened before checking description. Did not see same problems with scripts as I did in NZ. Maybe something to do with inserting timezone text.

--- cypress/integration/checklist/checklistUpdate.spec.js
+++ cypress/integration/checklist/checklistUpdate.spec.js
@@ -7,31 +7,31 @@ context("Update existing checklist", () => {
     cy.login(Cypress.env("testuser"), Cypress.env("password"));
   });
 
-    // Go Home
+  // Go Home
   it("01 Go home", () => {
     cy.get("#navHome").click();
     cy.contains("Streamline tasks with checklists");
   });
-    // Load my checklists
-    it("02 Load my checklists", () => {
+  // Load my checklists
+  it("02 Load my checklists", () => {
     cy.get("#navChecklists").click();
     cy.get("#navMyChecklists").click();
 
     cy.contains(checklistTitlePrefix).click();
-    });
+  });
 
-    // Check for checklist and checklist item to display
-    it("03 Open test checklist", () => {
+  // Check for checklist and checklist item to display
+  it("03 Open test checklist", () => {
     cy.url().should("include", "checklist/iqGoFXFTdZKtrQG3EZD9");
     cy.contains(checklistItemPrefix);
-    });
+  });
 
-    // Go to designer
-    it("04 Open and update in the designer", () => {
+  // Go to designer
+  it("04 Open and update in the designer", () => {
     cy.get("#headerRightButton").click();
     cy.wait(1002);
     cy.get("#title").type("{selectall}" + checklistTitlePrefix + " " + now);
-    
+
     cy.get("#description").within(() => {
       cy.get(".ngx-editor-textarea").type(
         "{selectall}Checklist Description: " + now
@@ -47,24 +47,25 @@ context("Update existing checklist", () => {
       );
     });
   });
-    // Go to the checklist and verify all fields are updated
+  // Go to the checklist and verify all fields are updated
 
-    it("05 Title updated", () => {
+  it("05 Title updated", () => {
     cy.get("#navChecklists").click();
     cy.get("#navMyChecklists").click();
 
     cy.contains(checklistTitlePrefix).click();
     cy.wait(1001);
     cy.contains(checklistTitlePrefix + " " + now); // Title
-    });
-    it("06 Description updated", () => {
+  });
+  it("06 Description updated", () => {
+    cy.get("#headerToggle").click(); // open checklists header to see details
     cy.contains("Checklist Description: " + now); // CL desc
-    });
-    it("07 Item Prompt updated", () => {
+  });
+  it("07 Item Prompt updated", () => {
     cy.get("#itemDropdownToggle").click(); // open item details
     cy.contains(checklistItemPrefix + " " + now); // Item prompt
-    });
-    it("08 tem Description Updated", () => {
+  });
+  it("08 tem Description Updated", () => {
     cy.contains("Item Description : " + now); // Item Desc
   });
 
